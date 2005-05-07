69787765910ea9bc6f79bb09d389b12054da3988
Marcin Konarski <marcin.konarski@codestation.org>
2005-05-07 05:32:15 +0000

    r796: Oracle database maintaince scripts ready.

--- data/create_oracle_booker.sql
+++ data/create_oracle_booker.sql
@@ -2,30 +2,39 @@
 
 The idea.
 
-We create user that will be owner of all stdhapi related
-databases with name 'h_db_owner'.
+Prerequirements: database instance is called `spoon'.
 
 We create role that has permissions to modify data in database
-(SELECT, INSERT, UPDATE, DELETE) with name 'dml_role'.
+(SELECT, INSERT, UPDATE, DELETE) with name `dml_role'.
 
-We create user with quota 0 so hi can not create any own objects
-with name 'booker' (this is application specific).
-We add role 'dml_role' to user 'booker'.
-We add access rights for user 'booker' to specyfic tables owned by
-'h_db_owner'.
+We create role that has permission to create users, and grant dml_role
+to them with name `ddl_role'.
+
+We create user that will be owner of all stdhapi related
+databases with name `root'.
+
+We grant ddl_role and dml_role to user `root'.
+
+We create user with quota 0 so he can not create his own objects
+with name `booker' (this is application specific).
+We add role `dml_role' on root's objects to user `booker'.
 
 */
 
-CREATE OR REPLACE USER h_db_owner
-	IDENTIFIED BY "hdb0wn3r"
+CREATE ROLE dml_role NOT IDENTIFIED;
+CREATE ROLE ddl_role NOT IDENTIFIED;
+GRANT CREATE USER, CREATE TABLE, CREATE ANY SYNONYM TO ddl_role;
+GRANT CREATE SESSION TO dml_role;
+CREATE USER root
+	IDENTIFIED BY "4dm;n1qs"
 	DEFAULT TABLESPACE "USERS"
 	QUOTA 4M ON "USERS"
 	ACCOUNT UNLOCK
 	TEMPORARY TABLESPACE "TEMP";
-CREATE OR REPLACE ROLE dml_role NOT IDENTIFIED;
-GRANT SELECT,INSERT,UPDATE,DELETE TO dml_role;
-GRANT CREATE SESSION TO dml_role;
-CREATE OR REPLACE USER booker
+GRANT ddl_role TO root;
+GRANT dml_role TO root WITH ADMIN OPTION;
+CONNECT root/"4dm;n1qs"@spoon
+CREATE USER booker
 	IDENTIFIED BY "b00k3r"
 	DEFAULT TABLESPACE "USERS"
 	QUOTA 0 ON "USERS"
@@ -40,5 +49,67 @@ for user 'booker'.
 
 */
 
-CREATE OR REPLACE TABLE config
- ... dalej nie wiem co.
+DROP TABLE dictionary_bank;
+DROP TABLE dictionary_contracting_party;
+DROP TABLE account_map;
+DROP TABLE config;
+
+CREATE TABLE config
+	(
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(16) UNIQUE NOT NULL,
+	integer_value NUMBER(4),
+	text_value VARCHAR(255),
+	float_value NUMBER(8,8),
+	time_value TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+	blob_value BLOB
+	);
+CREATE TABLE account_map
+	(
+	id INTEGER PRIMARY KEY,
+	id_account_map NUMBER(4) NOT NULL,
+	short_number VARCHAR(8),
+	full_number VARCHAR(32),
+	name VARCHAR(128)
+	);
+CREATE TABLE dictionary_contracting_party
+	(
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(32),
+	surname VARCHAR(32),
+	tin VARCHAR(16),
+	country VARCHAR(32),
+	city VARCHAR(32),
+	street VARCHAR(128),
+	street_number VARCHAR(8),
+	flat_number VARCHAR(8),
+	postal_code VARCHAR(8),
+	phone VARCHAR(32),
+	fax VARCHAR(32),
+	email VARCHAR(32),
+	id_account_map NUMBER(4)
+	);
+CREATE TABLE dictionary_bank
+	(
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(128),
+	country VARCHAR(32),
+	city VARCHAR(32),
+	street VARCHAR(128),
+	street_number VARCHAR(8),
+	postal_code VARCHAR(8),
+	phone VARCHAR(32),
+	fax VARCHAR(32)
+	);
+
+GRANT SELECT,INSERT,UPDATE,DELETE ON root.config TO dml_role;
+GRANT SELECT,INSERT,UPDATE,DELETE ON root.account_map TO dml_role;
+GRANT SELECT,INSERT,UPDATE,DELETE ON root.dictionary_bank TO dml_role;
+GRANT SELECT,INSERT,UPDATE,DELETE ON root.dictionary_contracting_party TO dml_role;
+CREATE SYNONYM booker.config FOR config;
+CREATE SYNONYM booker.account_map FOR account_map;
+CREATE SYNONYM booker.dictionary_bank FOR dictionary_bank;
+CREATE SYNONYM booker.dictionary_contracting_party FOR dictionary_contracting_party;
+/*}}}*/
+
+
---
+++ data/remove_oracle_booker.sql
@@ -0,0 +1,9 @@
+DROP TABLE root.account_map;
+DROP TABLE root.dictionary_contracting_party;
+DROP TABLE root.dictionary_bank;
+DROP TABLE root.config;
+
+DROP USER booker;
+DROP USER root;
+DROP ROLE dml_role;
+DROP ROLE ddl_role;
