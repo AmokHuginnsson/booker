b1b3ba1cc6cca40060e20d4582d5555e3c196147
Marcin Konarski <marcin.konarski@codestation.org>
2011-02-15 16:00:15 +0100

    congigure clog object.

--- src/setup.cxx
+++ src/setup.cxx
@@ -1,7 +1,7 @@
 /*
----       `booker' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski         ---
+---           `booker' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski            ---
 
-	setup.hxx - this file is integral part of `booker' project.
+	setup.cxx - this file is integral part of `booker' project.
 
 	i.  You may not make any changes in Copyright information.
 	ii. You must attach Copyright information to any part of every copy
@@ -24,39 +24,38 @@ Copyright:
  FITNESS FOR A PARTICULAR PURPOSE. Use it at your own risk.
 */
 
-#ifndef SETUP_HXX_INCLUDED
-#define SETUP_HXX_INCLUDED
+#include <cstdio>
 
-#include <libintl.h>
+#include <yaal/yaal.hxx>
+M_VCSID( "$Id: "__ID__" $" )
+#include "setup.hxx"
 
-#include "config.hxx"
+using namespace yaal::hcore;
 
 namespace booker
 {
 
-struct OSetup
+void OSetup::test_setup( void )
 	{
-	bool _quiet;			/* --quiet, --silent */
-	bool _verbose;		/* --verbose */
-	char * _programName;
-	yaal::hcore::HString _logPath;
-	yaal::hcore::HString _dataBase;
-	yaal::hcore::HString _login;
-	yaal::hcore::HString _password;
-	/* self-sufficient */
-	OSetup( void )
-		: _quiet( false ), _verbose( false ),
-		_programName( NULL ),
-		_logPath(), _dataBase(),
-		_login(), _password() {}
-	void test_setup( void );
-private:
-	OSetup ( OSetup const& );
-	OSetup& operator = ( OSetup const& );
-	};
-
-extern OSetup setup;
+	M_PROLOG
+	if ( _quiet && _verbose )
+		yaal::tools::util::failure( 1,
+				_( "quiet and verbose options are exclusive\n" ) );
+	if ( _verbose )
+		clog.reset( HStreamInterface::ptr_t( new HFile( stdout ) ) );
+	if ( _quiet )
+		cout.reset();
+	if ( ! _dataBase )
+		M_THROW( _( "database not set" ), errno );
+	if ( ! _login )
+		M_THROW( _( "database login not set" ), errno );
+	if ( ! _password )
+		M_THROW( _( "database password not set" ), errno );
+	if ( ! _logPath )
+		M_THROW( _( "log file name not set" ), errno );
+	return;
+	M_EPILOG
+	}
 
 }
 
-#endif /* SETUP_HXX_INCLUDED */
